96434e81bdadf28c1371ab69831dfe575bfc1b22
SINGH BOY <rohitsharma007@gmail.com>
2016-03-20 20:31:01 +0530

    user register

---
+++ Oodb.js
@@ -0,0 +1,89 @@
+var mongodb = require('./mongo_client'),
+    ObjectId = require('mongodb').ObjectID;
+    async = require('async')
+
+var oodb = function (){  
+   var self = this;
+
+
+   self.get = function (query,callback){
+   		id = new ObjectId()
+   		if(query.filter.id){
+   			query.filter._id = new ObjectId(query.filter.id)
+   			delete query.filter.id;
+   		}
+   		console.log(query);
+   		console.log(query.filter)
+   		 				    mongodb.db()
+                            .collection(query.tableName)
+                            .find(query.filter)
+                            .toArray(function(err, response) {
+                            if (err) {
+                            		 callback({status:500,msg:"some error occured"}) 
+                            		}
+                            		   callback({status:200, data:response})
+                            		   mongodb.close();
+                            });
+
+   };
+
+   self.save = function(query,callback){
+   		console.log(query)
+   		if(query.value.email){
+   			query.value._id = query.value.email;
+   			delete query.value.email;
+   		}
+   		
+
+
+			async.waterfall([fetchUser, saveUser], function(response){
+				callback(response)	
+			})
+
+
+		function fetchUser(callback){
+			console.log("fetchUser")
+			mongodb.db()
+	        .collection(query.tableName)
+	        .find({_id:query.value._id})
+	        .toArray(function(err, response) {
+	        if (err){
+	        		 callback({status:500,msg:"some error occured"}) 
+	        		}else{
+	        			console.log(response.length)
+	     	        	if(response.length == 0){
+	        				console.log("no user found")
+	        				saveUser(callback)		
+	        			}else{
+	        				console.log('user found')
+	        				callback({status:500,msg:"User Already exists"})			
+	        			}	        		 
+	        		}
+	        		  
+	        		 //  mongodb.close();
+	        });
+
+		}
+
+		function saveUser(callback){
+			console.log("saveUser")
+			 mongodb.db().collection(query.tableName).insertOne(query.value, function(err, response){
+   			  if(err){
+   			  	console.log("saveUser erro occured")
+   			  	console.log(err)
+   			  		callback({status:500, msg:"some erro occured"});
+   			  }else{
+   			  	console.log("saveUser success")
+   			      callback({status:200, data:response})
+        		  
+   			    }
+        		// mongodb.close();	
+   		});
+
+		}
+  
+   }
+
+};
+
+module.exports = oodb;
---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var mongodb = require('./mongo_client');
+var oodb = require('./Oodb.js');
+var bodyParser = require('body-parser')
+var _oodb = new oodb();  
+var url = 'mongodb://localhost:27017/test';
+
+
+// Use connect method to connect to the Server
+mongodb.connect(url, function(err, db) {
+  if (err) return
+  	console.log("mongo server connected...!!!!!")
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/', function(req, res) {
+  app.use(express.static(path.join(__dirname, 'www')));
+
+});
+
+app.post('/registerUser', function(req, res) {
+		var query = {
+			tableName : "users",
+			filter : {},
+			option :{},
+			value : req.body
+		};
+		_oodb.save(query,function(response){
+			if(response.status==200){
+				console.log(response.data)		
+				res.send({status:200, response: response.data})
+				res.status(200)
+			}else{
+				res.send({status:500, msg: response.msg})
+				res.status(500)
+				console.log(response.msg)
+				console.log("test")
+			}			
+		});
+		// _oodb.get(query,function(response){
+		// 	if(response.status==200){
+		// 		console.log(response.data)		
+		// 		res.send({status:200, response: response.data})
+		// 		res.status(200)
+		// 	}else{
+		// 		res.send({status:500, msg: response.msg})
+		// 		res.status(500)
+		// 		console.log(response.msg)
+		// 	}			
+		// });
+})
+
+app.listen(8080,'192.168.56.1');
+
+console.log("app running in localhost")
